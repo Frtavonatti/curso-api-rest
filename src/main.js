99514f0b597daf0593acda813965d52f67ab03e8
Frtavonatti <frtavonatti@gmail.com>
2024-02-04 13:09:41 -0300

    Agragada funcionalidad para agregar multiples imagenes

--- src/main.js
+++ src/main.js
@@ -1,28 +1,33 @@
-const URL = "https://api.thecatapi.com/v1/images/search"
-const image = document.querySelector("img")
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3"
+const API_KEY = "live_gTmJb4C012uusw2Ge6O8HeXMrl6YGYJPXWyxIRP4ugyUR9mhYvJ7dHI5fcQtjOsX"
+const KEY_EXAMPLE = ` https://api.thecatapi.com/v1/images/search?limit=3&breed_ids=beng&api_key=${API_KEY}` 
 
-//Solicitar data a la API con Promesas
+const image1 = document.querySelector("#img1")
+const image2 = document.querySelector("#img2")
+const image3 = document.querySelector("#img3")
+
+document.addEventListener("DOMContentLoaded", fetchData)
+
 // fetch(URL)
 // .then(response => response.json())
 // .then(data => {
 //     image.src = data[0].url
 // })
 
-//Solicitar data a la API con Async/Await
-async function fetchData() {
-    try {
-      const response = await fetch(URL);
-      const data = await response.json();
-      image.src = data[0].url;
-    } catch (error) {
-      console.error('Error al realizar la solicitud:', error.message);
-    }
+async function fetchData () {
+  try {
+      const response = await fetch(KEY_EXAMPLE)
+      const data = await response.json()
+      console.log(data)
+      image1.src = data[0].url
+      image2.src = data[1].url
+      image3.src = data[2].url
+  } catch (error) {
+      console.error(error)
   }
-  fetchData();
+}
 
-//Crear nuevo boton
 const button = document.createElement("button")
 button.innerText = "New Cat"
 document.body.appendChild(button)
-
 button.addEventListener("click", fetchData)
